50908bd36696099e6621c6a982036931596eb0cb
takujifunao <takuji.funao@gmail.com>
2016-02-17 12:08:05 +0900

    Initial Commit

---
+++ TestOpenCV/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TestOpenCV
+//
+//  Created by takujifunao on 2016/02/17.
+//  Copyright © 2016年 takujifunao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
